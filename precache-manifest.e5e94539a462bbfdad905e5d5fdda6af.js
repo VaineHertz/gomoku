0106ccc19ba8365dd0258be8d462221f9ca4715e
Tony Padilla <vainehertz@yahoo.com>
2022-04-12 15:30:00 -0700

    Updates

---
+++ precache-manifest.e5e94539a462bbfdad905e5d5fdda6af.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a77ea83255ceabb6a0e02ff20528ee5",
+    "url": "/gomoku/index.html"
+  },
+  {
+    "revision": "be1ef6b59b8dcf09cf9b",
+    "url": "/gomoku/static/css/main.63d666fc.chunk.css"
+  },
+  {
+    "revision": "7da278c41924b11db819",
+    "url": "/gomoku/static/js/2.6371ef25.chunk.js"
+  },
+  {
+    "revision": "63fbe989101a4b7a3114",
+    "url": "/gomoku/static/js/3.13844b13.chunk.js"
+  },
+  {
+    "revision": "be1ef6b59b8dcf09cf9b",
+    "url": "/gomoku/static/js/main.6e01ccb7.chunk.js"
+  },
+  {
+    "revision": "9c954c2807a7d68bd7db",
+    "url": "/gomoku/static/js/runtime~main.bcea9654.js"
+  },
+  {
+    "revision": "859a0d361f82ed6adb24057ef6cfa543",
+    "url": "/gomoku/static/media/GreatVibes-Regular.859a0d36.ttf"
+  }
+]);
