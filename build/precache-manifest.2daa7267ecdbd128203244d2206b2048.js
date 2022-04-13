d4b79756fdc10c6fa6d96c58614b1c5cffda3322
Tony Padilla <vainehertz@yahoo.com>
2022-04-13 15:25:32 -0700

    made pieces into circles

---
+++ build/precache-manifest.2daa7267ecdbd128203244d2206b2048.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cefecbb521f747742ab24f19ad54ea",
+    "url": "/gomoku/index.html"
+  },
+  {
+    "revision": "64a3d49e7668af54fb05",
+    "url": "/gomoku/static/css/main.2ed08d31.chunk.css"
+  },
+  {
+    "revision": "23e0a8d315680d51a46a",
+    "url": "/gomoku/static/js/2.2ebdc436.chunk.js"
+  },
+  {
+    "revision": "17cafacefc0671cbaf3e",
+    "url": "/gomoku/static/js/3.0a738eea.chunk.js"
+  },
+  {
+    "revision": "64a3d49e7668af54fb05",
+    "url": "/gomoku/static/js/main.0b8d18e0.chunk.js"
+  },
+  {
+    "revision": "a00e9656df5f64c3c500",
+    "url": "/gomoku/static/js/runtime~main.9ea63bea.js"
+  },
+  {
+    "revision": "859a0d361f82ed6adb24057ef6cfa543",
+    "url": "/gomoku/static/media/GreatVibes-Regular.859a0d36.ttf"
+  }
+]);
